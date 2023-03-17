b836d247cfa9db3028e46aed7d134ceda244b48c
tabur <tabur@DERNOVAPI>
2023-03-17 23:34:23 +0300

    Добавьте файлы проекта.

---
+++ Practika2/Page1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Practika2._Praktika1_1DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practika2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        CoffeeTableAdapter coffee = new CoffeeTableAdapter();
+        public Page1()
+        {
+            InitializeComponent();
+            CoffeeDataGrid.ItemsSource = coffee.GetData();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            coffee.InsertQuery(Name.Text, Convert.ToInt16(Razmer.Text), Convert.ToInt16(Price.Text));
+            CoffeeDataGrid.ItemsSource = coffee.GetData();
+
+
+        }
+
+    }
+}
---
+++ Practika2/Page2.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Practika2._Praktika1_1DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practika2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        Pocupatel_TableAdapter pokupatel = new Pocupatel_TableAdapter();
+        CoffeeTableAdapter coffee = new CoffeeTableAdapter();
+        public Page2()
+        {
+            InitializeComponent();
+            Pocupatel_DataGrid.ItemsSource = pokupatel.GetData();
+            coffeeID.ItemsSource = coffee.GetData();
+            coffeeID.DisplayMemberPath = "Name";
+            coffeeID.SelectedValuePath = "Id";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int id = (int)coffeeID.SelectedValue;
+            pokupatel.InsertQuery(Name.Text, id);
+
+            Pocupatel_DataGrid.ItemsSource = pokupatel.GetData();
+        }
+    }
+}
